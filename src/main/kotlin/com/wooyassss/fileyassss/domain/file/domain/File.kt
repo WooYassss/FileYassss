84530d086a0d99e20b94da37a9f34dc131bfd413
GGos3 <dev.ggos3@gmail.com>
2023-10-16 17:48:29 +0900

    feat: File 객체 추가

---
+++ src/main/kotlin/com/wooyassss/fileyassss/domain/file/domain/File.kt
@@ -0,0 +1,19 @@
+package com.wooyassss.fileyassss.domain.file.domain
+
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.data.annotation.Id
+import org.springframework.data.mongodb.core.mapping.Document
+import java.time.LocalDate
+
+@Document("Files")
+data class File(
+    @Id @Value("file_id")
+    val id: String? = null,
+    var name: String,
+    var signature: String,
+    var path: String,
+    val uploader: String,
+    // 시간 저장
+    val createdAt: LocalDate,
+    var updatedAt: LocalDate
+)
