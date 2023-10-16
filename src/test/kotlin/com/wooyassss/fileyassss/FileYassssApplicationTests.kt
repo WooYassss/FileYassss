e74ff975ba68dd33dc63cb7766c7ebd06962a68f
GGos3 <dev.ggos3@gmail.com>
2023-10-16 17:25:00 +0900

    chore: init

---
+++ src/test/kotlin/com/wooyassss/fileyassss/FileYassssApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.wooyassss.fileyassss
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FileYassssApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
