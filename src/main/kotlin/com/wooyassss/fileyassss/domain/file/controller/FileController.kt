b2fbafbf1ec132438992a275437d82e62c6e1843
GGos3 <janpll0421@gmail.com>
2023-10-17 23:39:04 +0900

    feat: 컨트롤러 추가

---
+++ src/main/kotlin/com/wooyassss/fileyassss/domain/file/controller/FileController.kt
@@ -0,0 +1,11 @@
+package com.wooyassss.fileyassss.domain.file.controller
+
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.asFlow
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class FileController {
+
+}
