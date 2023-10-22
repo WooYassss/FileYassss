dd31a2dbc5807df6068ac45f6efb3889bd4a4d41
GGos3 <janpll0421@gmail.com>
2023-10-23 00:04:27 +0900

    feat: 기본 값, 리턴 타입 추가

--- src/main/kotlin/com/wooyassss/fileyassss/domain/file/controller/FileController.kt
+++ src/main/kotlin/com/wooyassss/fileyassss/domain/file/controller/FileController.kt
@@ -1,10 +1,10 @@
 package com.wooyassss.fileyassss.domain.file.controller
 
+import com.wooyassss.fileyassss.domain.file.domain.FileInfo
 import com.wooyassss.fileyassss.domain.file.dto.reqeust.SaveFileRequest
 import com.wooyassss.fileyassss.domain.file.service.FileService
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.reactive.asFlow
-import org.reactivestreams.Publisher
 import org.springframework.http.codec.multipart.FilePart
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestHeader
@@ -20,12 +20,12 @@ class FileController(
 
     @PostMapping("/save")
     suspend fun saveFile(
-        @RequestHeader("user-name", required = false) uploader: String,
+        @RequestHeader("user-name", required = false) uploader: String?,
         @RequestPart("files") files: Flux<FilePart>
-    ): Flow<Void> {
-
-        return fileService.saveFile(
+    ): Flow<FileInfo> {
+         return fileService.saveFile(
             SaveFileRequest(
+                uploader = uploader ?: "Anonymous",
                 files = files.asFlow()
             )
         )
