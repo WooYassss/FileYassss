66a42829208bde3800384f24f963a5f137920d8d
GGos3 <dev.ggos3@gmail.com>
2023-10-24 17:54:00 +0900

    chore: 함수 순서 변경 및 파일ID 생성 함수 추가

--- src/main/kotlin/com/wooyassss/fileyassss/domain/file/service/FileServiceImpl.kt
+++ src/main/kotlin/com/wooyassss/fileyassss/domain/file/service/FileServiceImpl.kt
@@ -6,16 +6,19 @@ import com.wooyassss.fileyassss.domain.file.domain.FileInfoRepository
 import com.wooyassss.fileyassss.domain.file.dto.reqeust.SaveFileRequest
 import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.reactive.asFlow
-import kotlinx.coroutines.reactor.awaitSingle
 import kotlinx.coroutines.reactor.awaitSingleOrNull
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.http.codec.multipart.FilePart
 import org.springframework.stereotype.Service
-import reactor.core.publisher.Mono
 import java.io.File
 import java.nio.file.Path
+import java.sql.Timestamp
+import java.time.LocalDateTime
 import kotlin.io.path.fileSize
+import kotlin.random.Random
+import kotlin.random.nextInt
+
 @Service
 class FileServiceImpl(
     @Value("\${base.file.path}")
@@ -42,11 +45,33 @@ class FileServiceImpl(
     override fun saveFile(req: SaveFileRequest): Flow<FileInfo> =
         req.files.flatMapConcat { file ->
             // /$basePath/$uploader
-            val filePath = createPath(req.uploader).resolve(file.filename())
+            val fileId = createFileId()
+            val filePath = createPath(req.uploader).resolve(fileId)
 
             saveFilePart(file, filePath)
             saveFileInfo(file, filePath, req.uploader)
         }
+
+    private suspend fun createPath(path: String): Path {
+        val directory = File("$basePath/$path")
+
+        return if (directory.exists())
+            directory.toPath()
+        else {
+            log.info("Create Path={}", directory.canonicalPath)
+            directory.mkdir()
+            createPath(path)
+        }
+    }
+
+    private suspend fun createFileId(): String {
+        val timestamp = Timestamp.valueOf(LocalDateTime.now())
+        val random = Random(timestamp.time).nextInt(10..40)
+
+        val fileId = timestamp.time.toString() + random.toString()
+        log.info("createFileId ={}", fileId)
+        return fileId
+    }
 
     private suspend fun saveFilePart(
         file: FilePart,
@@ -70,17 +95,5 @@ class FileServiceImpl(
                 uploader = uploader,
             )
         ).asFlow()
-
-    private suspend fun createPath(path: String): Path {
-        val directory = File("$basePath/$path")
-
-        return if (directory.exists()) {
-            directory.toPath()
-        } else {
-            log.info("Create Path={}", directory.canonicalPath)
-            directory.mkdir()
-            createPath(path)
-        }
-    }
 }
 
